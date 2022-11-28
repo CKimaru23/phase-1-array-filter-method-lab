c09c5f67239cd0bab4b835bc293db9a3b3f4b741
Collins Kiragu <collins.kiragu@student.moringaschool.com>
2022-11-28 16:12:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(origin, find) {
+    return origin.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === find.toLowerCase()
+    )
+  }
+  findMatching(drivers, 'Bobby');
+  
+  function fuzzyMatch(origin, tstString) {
+    return origin.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(tstString.toLowerCase()) === 0
+    )
+  }
+
+function matchName(array,name) {
+    console.log(name)
+    console.log(array)
+let results = array.filter(object => object.name === name);
+console.log(results)
+return results
+}
